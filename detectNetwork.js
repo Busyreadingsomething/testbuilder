9e85c73127fab19b0ef61d4920df5a010561cf8c
Makan Azarshahy <oro1919@gmail.com>
2017-12-18 15:30:48 -0800

    Added Card types

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,6 +8,17 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
+
+	if ((cardNumber.slice(0, 2) === '38' || cardNumber.slice(0, 2) === '39') && cardNumber.length === 14) {
+		return "Diner's Club";
+	} else if ((cardNumber.slice(0, 2) === '34' || cardNumber.slice(0, 2) === '37') && cardNumber.length === 15) {
+		return "American Express";
+	} else if ((cardNumber[0] === '4') && (cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19)) {
+		return "Visa";
+	} else if ((cardNumber.slice(0, 2) === '51' || cardNumber.slice(0, 2) === '52' || cardNumber.slice(0, 2) === '53' || cardNumber.slice(0, 2) === '54' || cardNumber.slice(0, 2) === '55') && cardNumber.length === 16) {
+		return "MasterCard";
+	}
+
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
@@ -16,3 +27,7 @@ var detectNetwork = function(cardNumber) {
 };
 
 
+console.log(detectNetwork('38345678901234')); //(Diner's Club)
+console.log(detectNetwork('39345678901234'));
+console.log(detectNetwork('343456789012345'));
+console.log(detectNetwork('373456789012345'));
